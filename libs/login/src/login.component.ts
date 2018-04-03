ba0b35c173fd41244071f789f47c5a70acff6a56
Volodymyr Shevchuk <Volodymyr.Shevchuk@eleks.com>
2018-04-03 19:34:06 +0300

    Added styling for shared "login" lib;

--- libs/login/src/login.component.ts
+++ libs/login/src/login.component.ts
@@ -1,13 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+  FormControlName
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  isLoading = false;
+  loginForm: FormGroup;
+  loginFormSubmitted: boolean;
+  emptyFieldsError = 'Please enter all required fields';
+  copyrightYear = 'some date';
 
-  constructor() {}
+  get hasLoginError(): boolean {
+    const condition = this.loginFormSubmitted && !this.loginForm.valid;
 
-  ngOnInit() {}
+    return Boolean(condition);
+  }
+
+  get loginErrorMessage(): string {
+    return this.loginFormSubmitted && !this.loginForm.valid
+      ? this.emptyFieldsError
+      : 'this.loginResult.reason';
+  }
+
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      organization: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.loginFormSubmitted = true;
+
+    if (!this.loginForm.valid) {
+      return;
+    }
+    this.isLoading = true;
+  }
+
+  resetSubmitted() {
+    this.loginFormSubmitted = false;
+  }
 }
--- libs/login/src/login.module.ts
+++ libs/login/src/login.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from "@nx-experiments/login/src/login.component";
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {path: '', pathMatch: 'full', component: LoginComponent}
     ])
